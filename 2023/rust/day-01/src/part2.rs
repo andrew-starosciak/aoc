211e3e5ccab4db257d5d12e243f047eee48c8218
Andrew <andrewstarosciak@gmail.com>
2023-12-02 19:54:58 -0500

    feat(2023): day1 part2

--- 2023/rust/day-01/src/part2.rs
+++ 2023/rust/day-01/src/part2.rs
@@ -2,18 +2,79 @@ use crate::custom_error::AocError;
 
 #[tracing::instrument]
 pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    let output = _input
+        .lines()
+        .map(|characters| process_characters(characters))
+        .sum::<u32>();
+    Ok(output.to_string())
+}
+
+fn process_characters(characters: &str) -> u32 {
+    let mut index = 0;
+    let char_inter = std::iter::from_fn(move || {
+        let reduced_line = &characters[index..];
+
+        let result = if reduced_line.starts_with("one") {
+            Some('1')
+        } else if reduced_line.starts_with("two") {
+            Some('2')
+        } else if reduced_line.starts_with("three") {
+            Some('3')
+        } else if reduced_line.starts_with("four") {
+            Some('4')
+        } else if reduced_line.starts_with("five") {
+            Some('5')
+        } else if reduced_line.starts_with("six") {
+            Some('6')
+        } else if reduced_line.starts_with("seven") {
+            Some('7')
+        } else if reduced_line.starts_with("eight") {
+            Some('8')
+        } else if reduced_line.starts_with("nine") {
+            Some('9')
+        } else {
+            let result = reduced_line.chars().next();
+            result
+        };
+
+        index += 1;
+        result
+    });
+    let mut it = char_inter.filter_map(|char| char.to_digit(10));
+    let first = it.next().expect("to bea  number");
+
+    match it.last() {
+        Some(num) => format!("{first}{num}"),
+        None => format!("{first}{first}"),
+    }
+    .parse::<u32>()
+    .expect("should be number")
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    use rstest::rstest;
+    #[rstest]
+    #[case("two1nine", 29)]
+    #[case("eightwothree", 83)]
+    #[case("abcone2threexyz", 13)]
+    #[case("xtwone3four", 24)]
+    #[case("4nineeightseven2", 42)]
+    #[case("zoneight234", 14)]
+    #[case("7pqrstsixteen", 76)]
+    fn line_test(#[case] line: &str, #[case] expected: u32) {
+        assert_eq!(expected, process_characters(line))
+    }
+
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!("142", process(input)?);
         Ok(())
     }
 }
